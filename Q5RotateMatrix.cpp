191b4fe8cebe58cd9e86a8c2c977e5e17a9b1243
Gaurav Rana <24csu333@ncuindia.edu>
2025-02-08 22:01:38 +0530

    Q5RotateMatrix.cpp

--- Q5RotateMatrix.cpp
+++ Q5RotateMatrix.cpp
@@ -1,36 +1,39 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void rM(int m[][3], int n) {
-    for (int i = 0; i < n / 2; i++) {
-        for (int j = i; j < n - 1 - i; j++) {
-            int t = m[i][j];
-            m[i][j] = m[n - 1 - j][i];
-            m[n - 1 - j][i] = m[n - 1 - i][n - 1 - j];
-            m[n - 1 - i][n - 1 - j] = m[j][n - 1 - i];
-            m[j][n - 1 - i] = t;
-        }
+void rotateMatrix(int mat[][3], int n) {
+    for(int layer=0; layer<n/2; layer++)
+    for(int i=layer; i<n-1-layer; i++) {
+        int temp = mat[layer][i];
+        mat[layer][i] = mat[n-1-i][layer];
+        mat[n-1-i][layer] = mat[n-1-layer][n-1-i];
+        mat[n-1-layer][n-1-i] = mat[i][n-1-layer];
+        mat[i][n-1-layer] = temp;
     }
 }
 
-void pM(int m[][3], int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) cout << m[i][j] << " ";
+void displayMatrix(int mat[][3], int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++)
+            cout << mat[i][j] << " ";
         cout << endl;
     }
 }
 
 int main() {
-    int m[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int n = 3;
+    int mat[3][3], n=3;
+    cout << "Enter 3x3 matrix:\n";
+    for(int i=0; i<n; i++)
+    for(int j=0; j<n; j++)
+        cin >> mat[i][j];
 
-    cout << "Original:" << endl;
-    pM(m, n);
+    cout << "Before Rotation:\n";
+    displayMatrix(mat, n);
 
-    rM(m, n);
+    rotateMatrix(mat, n);
 
-    cout << "Rotated:" << endl;
-    pM(m, n);
+    cout << "After Rotation:\n";
+    displayMatrix(mat, n);
 
     return 0;
 }
